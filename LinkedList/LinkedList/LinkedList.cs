57197364d5fe50cd72699698dfe31c67d1ba507d
Kasatkina_Anna <Akasatkina904@gmail.com>
2025-04-03 17:43:15 +0300

    Add task

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,93 @@
+﻿// <copyright file="LinkedList.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace LinkedList;
+
+/// <summary>
+/// Represents a singly linked list with methods to manipulate the list.
+/// </summary>
+public class LinkedList
+{
+    /// <summary>
+    /// Gets the head node of the linked list.
+    /// </summary>
+    public LinkedListNode? Head { get; private set; }
+
+    /// <summary>
+    /// Gets the tail node of the linked list.
+    /// </summary>
+    public LinkedListNode? Tail { get; private set; }
+
+    /// <summary>
+    /// Adds a new node with the specified value to the end of the linked list.
+    /// </summary>
+    /// <param name="value">The value to add to the linked list.</param>
+    public void Add(int value)
+    {
+        var newNode = new LinkedListNode(value);
+        if (this.Head == null)
+        {
+            this.Head = newNode;
+            this.Tail = newNode;
+        }
+        else
+        {
+            this.Tail!.Next = newNode;
+            this.Tail = newNode;
+        }
+    }
+
+    /// <summary>
+    /// Removes consecutive duplicate elements from the linked list.
+    /// </summary>
+    /// <remarks>
+    /// This method modifies the linked list such that no two consecutive nodes have the same value.
+    /// </remarks>
+    public void RemoveDublicates()
+    {
+        if (this.Head == null || this.Head.Next == null)
+        {
+            return;
+        }
+
+        LinkedListNode previous = this.Head;
+        LinkedListNode? current = previous.Next;
+
+        while (current != null)
+        {
+            if (current.Value == previous.Value)
+            {
+                previous.Next = current.Next;
+                current = current.Next;
+
+                if (current == null)
+                {
+                    this.Tail = previous;
+                }
+            }
+            else
+            {
+                previous = current;
+                current = current.Next;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Converts the linked list to an array of integers.
+    /// </summary>
+    /// <returns>An array containing the values of the linked list in order.</returns>
+    public int[] ToArray()
+    {
+        List<int> result = new List<int>();
+        LinkedListNode? current = this.Head;
+        while (current != null)
+        {
+            result.Add(current.Value);
+            current = current.Next;
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ LinkedList/LinkedListTests/LinkedListTests.cs
@@ -0,0 +1,115 @@
+﻿// <copyright file="LinkedListTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace LinkedList.Tests;
+
+/// <summary>
+/// Contains unit tests for the <see cref="LinkedList"/> class.
+/// </summary>
+[TestFixture]
+public class LinkedListTests
+{
+    /// <summary>
+    /// Tests that removing duplicates from an empty list leaves it unchanged.
+    /// </summary>
+    [Test]
+    public void EmptyList()
+    {
+        var list = new LinkedList();
+        list.RemoveDublicates();
+        Assert.That(list.Head, Is.Null);
+        Assert.That(list.Tail, Is.Null);
+    }
+
+    /// <summary>
+    /// Tests that removing duplicates from a single-element list leaves it unchanged.
+    /// </summary>
+    [Test]
+    public void SingleElement()
+    {
+        var list = new LinkedList();
+        list.Add(5);
+        list.RemoveDublicates();
+        Assert.That(list.Head!.Value, Is.EqualTo(5));
+        Assert.That(list.Tail!.Value, Is.EqualTo(5));
+        Assert.That(list.Head.Next, Is.Null);
+    }
+
+    /// <summary>
+    /// Tests that consecutive duplicates are correctly removed from the list.
+    /// </summary>
+    [Test]
+    public void MultipleDuplicates()
+    {
+        var list = new LinkedList();
+        int[] input = { 1, 1, 2, 3, 3, 3 };
+        foreach (int num in input)
+        {
+            list.Add(num);
+        }
+
+        list.RemoveDublicates();
+
+        int[] expected = { 1, 2, 3 };
+        Assert.That(list.ToArray(), Is.EqualTo(expected));
+        Assert.That(list.Tail!.Value, Is.EqualTo(3));
+    }
+
+    /// <summary>
+    /// Tests that when all elements are the same, only one remains after removal.
+    /// </summary>
+    [Test]
+    public void AllElementsSame()
+    {
+        var list = new LinkedList();
+        list.Add(2);
+        list.Add(2);
+        list.Add(2);
+
+        list.RemoveDublicates();
+
+        Assert.That(list.Head!.Value, Is.EqualTo(2));
+        Assert.That(list.Tail!.Value, Is.EqualTo(2));
+        Assert.That(list.Head.Next, Is.Null);
+    }
+
+    /// <summary>
+    /// Tests that a list without duplicates remains unchanged.
+    /// </summary>
+    [Test]
+    public void NoDuplicates()
+    {
+        var list = new LinkedList();
+        int[] input = { 1, 2, 3, 4 };
+        foreach (int num in input)
+        {
+            list.Add(num);
+        }
+
+        list.RemoveDublicates();
+
+        Assert.That(list.ToArray(), Is.EqualTo(input));
+        Assert.That(list.Tail!.Value, Is.EqualTo(4));
+    }
+
+    /// <summary>
+    /// Tests that duplicates at the end of the list are correctly handled.
+    /// </summary>
+    [Test]
+    public void DuplicatesAtEnd()
+    {
+        var list = new LinkedList();
+        list.Add(1);
+        list.Add(2);
+        list.Add(2);
+        list.Add(2);
+
+        list.RemoveDublicates();
+
+        Assert.That(list.Tail!.Value, Is.EqualTo(2));
+        Assert.That(list.Tail.Next, Is.Null);
+        Assert.That(list.Head!.Value, Is.EqualTo(1));
+        Assert.That(list.Head.Next!.Value, Is.EqualTo(2));
+    }
+}
